cc6fd4ce74fa7a21a72bcace0ff01e1ce3443f7d
Joong Hyeon Kim <karis0222@gmail.com>
2016-11-02 15:00:17 +0900

    파일 위치 변경에 따른 코드 수정.

--- commands/command.py
+++ commands/command.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 import requests
 import secrets
 
-city_xy = open('city_xy.json', 'r', encoding='utf-8')
+city_xy = open('../city_xy.json', 'r', encoding='utf-8')
 CITY_XY = json.loads(city_xy.read())
 city_xy.close()
 
